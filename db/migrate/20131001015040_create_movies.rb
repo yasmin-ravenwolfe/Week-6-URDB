071249dc01272fe013c93e207f8d3337a0421854
Ricardo Cervantes <ricardo07@gmail.com>
2013-10-02 16:37:45 -0500

    Modify migration and create seeds for Movies

--- db/migrate/20131001015040_create_movies.rb
+++ db/migrate/20131001015040_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
       t.string :title
-      t.text :gif_url
+      t.text :poster_url
       t.text :trailer_url
       t.text :description
 
