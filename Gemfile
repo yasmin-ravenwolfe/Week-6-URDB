911d6935b3f74da47ff1505ca7c5186d0ab3330d
Matt Buck <github@matthewbuck.com>
2013-10-02 16:46:25 -0500

    Add a bit of base styling

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :doc do
 end
 
 gem 'zurb-foundation'
+gem 'haml-rails'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Urdb::Application.routes.draw do
 
+  resources :movies
+
   root 'movies#index'
-
-  get '/movies' => 'movies#index', as: 'movies'
-  get '/movies/:id' => 'movies#show', as: 'movie'
-
 end
