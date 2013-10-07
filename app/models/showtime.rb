0e5b8a099417f37cf2f997cbf8efaf043c70a852
Michael Ornellas <mike@themakersquare.com>
2013-10-07 01:03:44 -0500

    testing

--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -1,4 +1,3 @@
 class Showtime < ActiveRecord::Base
-  validates :location, presence: true
   belongs_to :movie
 end
--- spec/controllers/showtimes_controller_spec.rb
+++ spec/controllers/showtimes_controller_spec.rb
@@ -2,23 +2,25 @@ require 'spec_helper'
 
 describe ShowtimesController do
   let(:valid_attributes) { { "time" => "10/27/13 5:00pm" } }
-
+  before(:each) do
+    @movie = Movie.create
+  end
   describe "POST create" do
     context "with valid params" do
       it "creates a new Showtime" do
         expect {
-          post :create, {:showtime => valid_attributes}
+          post :create, {:movie_id => @movie.id, :showtime => valid_attributes}
         }.to change(Showtime, :count).by(1)
       end
 
       it "assigns a newly created showtime as @showtime" do
-        post :create, {:showtime => valid_attributes}
+        post :create, {:movie_id => @movie.id, :showtime => valid_attributes}
         expect(assigns(:showtime)).to be_a(Showtime)
         expect(assigns(:showtime)).to be_persisted
       end
 
       it "redirects to the created showtime's movie" do
-        post :create, {:showtime => valid_attributes}
+        post :create, {:movie_id => @movie.id, :showtime => valid_attributes}
         expect(response).to redirect_to(Showtime.last.movie)
       end
     end
@@ -28,15 +30,15 @@ describe ShowtimesController do
         # Trigger the behavior that occurs when invalid params are submitted
         allow_any_instance_of(Showtime).to receive(:save).and_return(false)
 
-        post :create, {:showtime => { "location" => "invalid value" }}
+        post :create, {:movie_id => @movie.id, :showtime => { "location" => "invalid value" }}
         expect(assigns(:showtime)).to be_a_new(Showtime)
       end
 
       it "re-renders the 'movies#show' template" do
         # Trigger the behavior that occurs when invalid params are submitted
         allow_any_instance_of(Showtime).to receive(:save).and_return(false)
-        post :create, {:showtime => { "location" => "invalid value" }}
-        expect(response).to render_template("movies/show")
+        post :create, {:movie_id => @movie.id, :showtime => { "location" => "invalid value" }}
+        expect(response).to redirect_to(@movie)
       end
     end
   end 
