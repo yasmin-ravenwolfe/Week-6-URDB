26e2ed5de07cad80a50176683690f800a524cde8
Michael Ornellas <mike@themakersquare.com>
2013-10-06 22:50:11 -0500

    moving spec file to new syntax

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -23,7 +23,7 @@ describe MoviesController do
   # This should return the minimal set of attributes required to create a valid
   # Movie. As you add validations to Movie, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { {  } }
+  let(:valid_attributes) { { "title" => "Citizen Kane" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
@@ -32,32 +32,32 @@ describe MoviesController do
 
   describe "GET index" do
     it "assigns all movies as @movies" do
-      movie = Movie.create! valid_attributes
-      get :index, {}, valid_session
-      assigns(:movies).should eq([movie])
+      movie = Movie.create valid_attributes
+      get :index
+      expect(assigns(:movies)).to eq([movie])
     end
   end
 
   describe "GET show" do
     it "assigns the requested movie as @movie" do
-      movie = Movie.create! valid_attributes
-      get :show, {:id => movie.to_param}, valid_session
-      assigns(:movie).should eq(movie)
+      movie = Movie.create valid_attributes
+      get :show, {:id => movie}
+      expect(assigns(:movie)).to eq(movie)
     end
   end
 
   describe "GET new" do
     it "assigns a new movie as @movie" do
-      get :new, {}, valid_session
-      assigns(:movie).should be_a_new(Movie)
+      get :new
+      expect(assigns(:movie)).to be_a_new(Movie)
     end
   end
 
   describe "GET edit" do
     it "assigns the requested movie as @movie" do
-      movie = Movie.create! valid_attributes
-      get :edit, {:id => movie.to_param}, valid_session
-      assigns(:movie).should eq(movie)
+      movie = Movie.create
+      get :edit, {:id => movie}
+      expect(assigns(:movie)).to eq(movie)
     end
   end
 
@@ -65,35 +65,35 @@ describe MoviesController do
     describe "with valid params" do
       it "creates a new Movie" do
         expect {
-          post :create, {:movie => valid_attributes}, valid_session
+          post :create, {:movie => valid_attributes}
         }.to change(Movie, :count).by(1)
       end
 
       it "assigns a newly created movie as @movie" do
-        post :create, {:movie => valid_attributes}, valid_session
-        assigns(:movie).should be_a(Movie)
-        assigns(:movie).should be_persisted
+        post :create, {:movie => valid_attributes}
+        expect(assigns(:movie)).to be_a(Movie)
+        expect(assigns(:movie)).to be_persisted
       end
 
       it "redirects to the created movie" do
-        post :create, {:movie => valid_attributes}, valid_session
-        response.should redirect_to(Movie.last)
+        post :create, {:movie => valid_attributes}
+        expect(response).to redirect_to(Movie.last)
       end
     end
 
     describe "with invalid params" do
       it "assigns a newly created but unsaved movie as @movie" do
         # Trigger the behavior that occurs when invalid params are submitted
-        Movie.any_instance.stub(:save).and_return(false)
-        post :create, {:movie => {  }}, valid_session
-        assigns(:movie).should be_a_new(Movie)
+        allow_any_instance_of(Movie).to receive(:save).and_return(false)
+        post :create, {:movie => {  }}
+        expect(assigns(:movie)).to be_a_new(Movie)
       end
 
       it "re-renders the 'new' template" do
         # Trigger the behavior that occurs when invalid params are submitted
-        Movie.any_instance.stub(:save).and_return(false)
-        post :create, {:movie => {  }}, valid_session
-        response.should render_template("new")
+        allow_any_instance_of(Movie).to receive(:save).and_return(false)
+        post :create, {:movie => {  }}
+        expect(response).to render_template("new")
       end
     end
   end
@@ -101,59 +101,59 @@ describe MoviesController do
   describe "PUT update" do
     describe "with valid params" do
       it "updates the requested movie" do
-        movie = Movie.create! valid_attributes
+        movie = Movie.create
         # Assuming there are no other movies in the database, this
         # specifies that the Movie created on the previous line
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
-        Movie.any_instance.should_receive(:update).with({ "these" => "params" })
-        put :update, {:id => movie.to_param, :movie => { "these" => "params" }}, valid_session
+        expect_any_instance_of(Movie).to receive(:update).with({ "title" => "King Kong" })
+        put :update, {:id => movie, :movie => { "title" => "King Kong" }}
       end
 
       it "assigns the requested movie as @movie" do
-        movie = Movie.create! valid_attributes
-        put :update, {:id => movie.to_param, :movie => valid_attributes}, valid_session
-        assigns(:movie).should eq(movie)
+        movie = Movie.create
+        put :update, {:id => movie, :movie => valid_attributes}
+        expect(assigns(:movie)).to eq(movie)
       end
 
       it "redirects to the movie" do
-        movie = Movie.create! valid_attributes
-        put :update, {:id => movie.to_param, :movie => valid_attributes}, valid_session
-        response.should redirect_to(movie)
+        movie = Movie.create
+        put :update, {:id => movie, :movie => valid_attributes}
+        expect(response).to redirect_to(movie)
       end
     end
 
     describe "with invalid params" do
       it "assigns the movie as @movie" do
-        movie = Movie.create! valid_attributes
+        movie = Movie.create
         # Trigger the behavior that occurs when invalid params are submitted
-        Movie.any_instance.stub(:save).and_return(false)
-        put :update, {:id => movie.to_param, :movie => {  }}, valid_session
-        assigns(:movie).should eq(movie)
+        allow_any_instance_of(Movie).to receive(:save).and_return(false)
+        put :update, {:id => movie, :movie => {  }}
+        expect(assigns(:movie)).to eq(movie)
       end
 
       it "re-renders the 'edit' template" do
-        movie = Movie.create! valid_attributes
+        movie = Movie.create
         # Trigger the behavior that occurs when invalid params are submitted
-        Movie.any_instance.stub(:save).and_return(false)
-        put :update, {:id => movie.to_param, :movie => {  }}, valid_session
-        response.should render_template("edit")
+        allow_any_instance_of(Movie).to receive(:save).and_return(false)
+        put :update, {:id => movie, :movie => {  }}
+        expect(response).to render_template("edit")
       end
     end
   end
 
   describe "DELETE destroy" do
     it "destroys the requested movie" do
-      movie = Movie.create! valid_attributes
+      movie = Movie.create
       expect {
         delete :destroy, {:id => movie.to_param}, valid_session
       }.to change(Movie, :count).by(-1)
     end
 
     it "redirects to the movies list" do
-      movie = Movie.create! valid_attributes
-      delete :destroy, {:id => movie.to_param}, valid_session
-      response.should redirect_to(movies_url)
+      movie = Movie.create
+      delete :destroy, {:id => movie}
+      expect(response).to redirect_to(movies_url)
     end
   end
 
