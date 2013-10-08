59fc3c28ffb417abe73e35cbf4776030d9335fd3
Alex Levine <a17levine@gmail.com>
2013-10-08 14:28:22 -0500

    finished seed file for stars and roles, adding 500 days of summer and yes man

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,6 +37,52 @@ movies = [
   }
 ]
 
+
+
+# ---------- day 3 seeds!!!
+
 movies.each do |movie|
   Movie.create(movie)
 end
+
+days_of_summer = Movie.create({
+    title: "(500) Days of Summer",
+    poster_url: "http://upload.wikimedia.org/wikipedia/en/thumb/d/d1/Five_hundred_days_of_summer.jpg/220px-Five_hundred_days_of_summer.jpg",
+    trailer_url: "http://youtu.be/PsD0NpFSADM",
+    description: "An offbeat romantic comedy about a woman who doesn't believe true love exists, and the young man who falls for her."
+  })
+
+yes_man = Movie.create({
+    title: "Yes Man",
+    poster_url: "http://upload.wikimedia.org/wikipedia/en/thumb/7/71/YesMan2008poster.jpg/220px-YesMan2008poster.jpg",
+    trailer_url: "http://youtu.be/rvpsiIe2vBE",
+    description: "A guy challenges himself to say 'yes' to everything for an entire year."
+  })
+
+
+
+
+jim_carrey = Star.create(name: 'Jim Carrey',
+                      headshot_url: 'http://upload.wikimedia.org/wikipedia/commons/8/8b/Jim_Carrey_2008.jpg',
+                      bio: 'Jim Carrey, a Canadian-born actor who became a naturalized American citizen in 2004, is an actor and producer famous for his rubbery body movements and flexible facial expressions.')
+
+zooey = Star.create(name: 'Zooey Deschanel',
+                      headshot_url: 'http://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Zooey_Deschanel_2009.jpg/220px-Zooey_Deschanel_2009.jpg',
+                      bio: 'Zooey Deschanel writes music, sings, and acts on screens, small and big. She is one half of the indie folk band She & Him.')
+
+joseph = Star.create(name: 'Joseph Gordon-Levitt',
+                      headshot_url: 'http://upload.wikimedia.org/wikipedia/commons/3/39/Joseph_Gordon-Levitt_TIFF_2012.jpg',
+                      bio: 'Joseph Gordon-Levitt is an American actor, director, producer, and writer. As a child star, he appeared in the films A River Runs Through It, Angels in the Outfield, Beethoven, and 10 Things I Hate About You, and as Tommy Solomon in the TV series 3rd Rock from the Sun.')
+
+
+yes_man.roles.create(character: 'Carl', star: jim_carrey)
+yes_man.roles.create(character: 'Allison', star: zooey)
+
+days_of_summer.roles.create(character: 'Tom', star: joseph)
+days_of_summer.roles.create(character: 'Summer', star: zooey)
+
+
+
+
+
+
