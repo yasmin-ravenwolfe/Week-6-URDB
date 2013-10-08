d3b961de91b2181a669da5833bc0648512c94907
Ricardo Cervantes <ricardo07@gmail.com>
2013-10-08 13:43:05 -0500

    removed unnecessary 'self' from snippet method in movie mondel

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,8 @@ class Movie < ActiveRecord::Base
   has_many :showtimes
   has_many :roles
   has_many :stars, through: :roles
-  
+
   def snippet
-    self.description.truncate 50
+    description.truncate 50
   end
 end
