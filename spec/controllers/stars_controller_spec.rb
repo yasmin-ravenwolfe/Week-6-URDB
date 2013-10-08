2c0a614a8f67948ecc57cafa14a01625866c19d7
Matt Buck <github@matthewbuck.com>
2013-10-08 14:13:19 -0500

    Get tests in place

--- spec/controllers/stars_controller_spec.rb
+++ spec/controllers/stars_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe StarsController do
+  describe "GET show" do
+    it "assigns the requested star as @star" do
+      star = Star.create
+      get :show, {:id => star}
 
+      expect(assigns(:star)).to eq(star)
+    end
+  end
 end
