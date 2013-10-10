096ff662c1c6cdccc13d9ad8fc2b0c065fc4ea02
MakerSquare Student <students@themakersquare.com>
2013-10-10 10:51:29 -0500

    Add passing testing for audience rating method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,12 +2,22 @@ class Movie < ActiveRecord::Base
   has_many :showtimes
   has_many :roles
   has_many :stars, through: :roles
+  validates :title, presence: true
 
   def rotten_finder
-    RottenMovie.find(title: title, limit: 1)
+    rotten_finder_return = @movie =  RottenMovie.find(title: title, limit: 1)
   end
   
   def snippet
-    description.truncate 50
+    description.to_s.truncate 50
+  end
+
+  def audience_rating
+    rotten_finder_return = self.rotten_finder
+    if rotten_finder_return == []
+      return nil
+    else
+      rotten_finder_return.ratings.audience_score
+    end
   end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,57 @@
 require 'spec_helper'
 
 describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+    # use .new so we don't save to database- which slows down tests
+    let(:movie){Movie.new}
+    let(:rotten_finder_return){OpenStruct.new(ratings: OpenStruct.new(audience_score:84))}
+
+  describe "#snippet" do
+
+    context "when description is less than or equal to 50 characters" do
+        it "returns the full description" do
+        # movie.stub(:description).and_return("When a Roman general is betrayed and his family is")
+        
+        allow(movie).to receive(:description).and_return("When a Roman general is betrayed and his family is")
+        expect(movie.snippet).to eq("When a Roman general is betrayed and his family is")
+        end
+    end
+
+    context "when description is greater than 50 characters" do
+        it "returns the first 47 characters and appends '...'" do
+         allow(movie).to receive(:description).and_return("When a Roman general is betrayed and his family murdered by an emperor's corrupt son, he comes to Rome as a gladiator to seek revenge.")
+
+        
+          expect(movie.snippet).to eq("When a Roman general is betrayed and his family...")
+        end
+      end
+
+    context "when description is nil" do
+      it "should return an empty string" do
+
+        allow(movie).to receive(:description).and_return(nil)
+
+        expect(movie.snippet).to eq("")
+      end
+    end
+  end
+
+  describe "#audience_rating" do
+    
+    context "when Movie is found on Rotten Tomatoes" do
+      it "returns the audience score for the Movie" do
+        allow(movie).to receive(:rotten_finder).and_return(rotten_finder_return)
+
+        expect(movie.audience_rating).to eq(84)
+      end
+    end
+
+    context "when Movie is not found on Rotten Tomatoes" do
+      it "returns an empty array" do
+        allow(movie).to receive(:rotten_finder).and_return([])
+
+        expect(movie.audience_rating).to eq(nil)
+      end
+    end
+  end
 end
+
